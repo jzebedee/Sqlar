35103ac8d23cc703fe9a96afde4ce204c12ec873
J. Zebedee <zebedee@code.gripe>
2022-02-05 15:56:42 -0600

    Add SqlarGetFileCompressed test

--- test/dufs-data.Tests/SqlarTests.cs
+++ test/dufs-data.Tests/SqlarTests.cs
@@ -53,5 +53,27 @@ namespace dufs_data.Tests
             Assert.Equal(expected.sz, actual.sz);
             Assert.Equal(expected.data, actual.data);
         }
+
+        [Fact]
+        public void SqlarGetFileCompressed()
+        {
+            const string lipsum = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ullamcorper metus tellus, vel faucibus lorem egestas a. In hac habitasse platea dictumst. Sed fermentum dignissim sapien, maximus faucibus orci efficitur et. Ut tristique luctus lacus aliquam varius. Aliquam ullamcorper semper libero a tincidunt. Curabitur in diam tincidunt, ultricies ante id, auctor magna. Quisque rhoncus scelerisque mi, interdum lobortis felis porttitor nec. Maecenas molestie non mauris in efficitur. Maecenas eu rhoncus arcu. Ut dapibus placerat risus, in efficitur massa consequat sed. Morbi dapibus laoreet eros, vitae dapibus turpis cursus id. Donec sem elit, consequat facilisis vehicula eu, euismod vel nisl. Maecenas ligula odio, luctus vitae sollicitudin quis, porttitor quis orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum porta posuere neque eget interdum.";
+
+            using var conn = GetConnection();
+            using var sqlar = new Sqlar(conn);
+
+            byte[] text = Encoding.UTF8.GetBytes(lipsum);
+            SqlarFile expected = new("lipsum.txt", 0, 0, text.Length, text);
+
+            sqlar.Add(expected);
+
+            SqlarFile actual = sqlar[expected.name];
+            Assert.True(actual.IsCompressed);
+
+            var uncompressed = actual.Decompress();
+            Assert.False(uncompressed.IsCompressed);
+
+            Assert.Equal(text, uncompressed.data);
+        }
     }
 }
